f7c36780b60677a894669cc8927a27f09e7c5a55
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-08 21:34:10 -0500

    wip: views action for products and routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contactus(request):
+    return render(request, 'contactus.html')
+
+def location(request):
+    return render(request, 'location.html')
+
+def products_index(request):
+    pass
+
+def products_detail(request):
+    pass
+
+
+""""""""""""""""""
+
+class ProductCreate(CreateView):
+    pass
+
+class ProductUpdate(UpdateView):
+    pass
+
+class ProductDelete(DeleteView):
+    pass
