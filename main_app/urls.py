113c8cf5a5a7cac2f6e6202de3c604dbedb5970e
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-09 18:12:58 -0500

    wip: fixed issues in attr for views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [ 
 	path('', views.home, name='home'),
 	path('about/', views.about, name='about'),
-	path('contactus/', views.contact, name='contactus'),
+	path('contactus/', views.contactus, name='contactus'),
 	path('location/', views.location, name='location'),
 	path('products/', views.products_index, name="index"),
 	
@@ -13,5 +13,5 @@ urlpatterns = [
 	path('products/<int:pk>/update/', views.ProductUpdate.as_view(), name='products_update'),
 	path('products/<int:pk>/delete/', views.ProductDelete.as_view(), name='products_delete'),
 
-	path('account/signup/', views.signup, name='signup')
+	# path('account/signup/', views.signup, name='signup')
 ]
