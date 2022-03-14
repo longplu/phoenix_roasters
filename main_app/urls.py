3353362dcb6d0f0bf46f952b3f6aa23c0ec49a63
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-14 18:58:45 -0400

    wip: models refactored/shoppingcarts paths/functionality for modifying shoppingcarts

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,18 +1,21 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [ 
 	path('', views.home, name='home'),
 	path('about/', views.about, name='about'),
 	path('contactus/', views.contactus, name='contactus'),
 	path('location/', views.location, name='location'),
-	path('shoppingcart/', views.shoppingcart, name='shooppingcart'),
-	path('products/', views.products_index, name="index"),
 	
+	path('products/', views.products_index, name="index"),
 	path('products/<int:product_id>/', views.products_detail, name="detail"),
-	path('products/create/', views.ProductCreate.as_view(), name='product_create'),
+	path('products/create/', views.ProductCreate.as_view(), name='products_create'),
 	path('products/<int:pk>/update/', views.ProductUpdate.as_view(), name='products_update'),
 	path('products/<int:pk>/delete/', views.ProductDelete.as_view(), name='products_delete'),
 
-	path('account/signup/', views.signup, name='signup')
+	path('shoppingcarts/', views.shoppingcarts_index, name='shoppingcarts_index'),
+	path('shoppingcarts/add/<item_id>/', views.shoppingcarts_add, name='shoppingcarts_add'),
+	path('shoppingcarts/update/<item_id>/', views.shoppingcarts_update, name='shoppingcarts_update'),
+
+	path('account/signup', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UpdateForm, SignupForm
+from .models import *
 
-from .models import Product, ShoppingCart
 
 # Create your views here.
 def home(request):
@@ -19,37 +23,62 @@ def contactus(request):
 def location(request):
     return render(request, 'location.html')
 
-def shoppingcart(request):
-    pass
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        signup_form = SignupForm(request.POST)
+        if signup_form.is_valid():
+            signup_form.save()
+            username = signup_form.get('username')
+            messages.success(
+                request, "You have successfully created an account!")
+            login(request, username)
+            return redirect('home')
+    else:
+        signup_form = SignupForm()
+    return render(request, 'registration/singup.html', context={"signup_form": signup_form})
+
+
+def shoppingcarts_index(request):
+    return render(request, 'shoppingcarts/index.html')
+
+def shoppingcarts_add(request):
+    quantity = int(request.POST.get('quantity'))
+    shoppingcart = request.session.get('shoppingcarts_index', {})
+    if id in shoppingcart:
+        shoppingcart[id] = int(shoppingcart[id] + quantity)
+    else:
+        shoppingcart[id] = shoppingcart.get(id, quantity)
+    request.session['shoppingcarts_index'] = shoppingcart
+    return redirect(reverse('shoppingcarts_index'))
+
+def shoppingcarts_update(request):
+    quantity = int(request.POST.get('quantity'))
+    shoppingcart = request.session.get('shoppingcarts_index', {})
+    if quantity > 0:
+        shoppingcart[id] = quantity
+    else:
+        shoppingcart.pop(id)
+
+    request.session['shoppingcarts_index'] = shoppingcart
+    return redirect(reverse('shoppingcarts_index'))
 
 def products_index(request):
     products = Product.objects.all()
-    return render(request, 'products/index.html', {
-        'products': products,
+    return render(request, 'products.html', {
+        'products': products
     })
 
-def products_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
+def products_detail(request, id):
+    product = Product.objects.get(pk=id)
     return render(request, 'products/detail.html', {
         'product': product,
     })
 
-def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-        else:
-            error_message = 'invalid sign up - please try again'
-    form = UserCreationForm()
-    context = {'form':form, 'error':error_message}
-    return render(request, 'registration/signup.html', context)
-
-
-""""""""""""""""""
+""""""""""""""""""""""""""
 
 class ProductCreate(CreateView):
     pass
