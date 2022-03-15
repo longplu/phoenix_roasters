6c56db1ee001dfcde88b20d6708fac0ceb2a5895
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-15 17:26:11 -0400

    wip: path to checkout page

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,5 +17,7 @@ urlpatterns = [
 	path('shoppingcarts/add/<item_id>/', views.shoppingcarts_add, name='shoppingcarts_add'),
 	path('shoppingcarts/update/<item_id>/', views.shoppingcarts_update, name='shoppingcarts_update'),
 
+	path('checkouts/', views.checkouts_index, name='checkouts_index'),
+
 	path('account/signup', views.signup, name='signup')
 ]
