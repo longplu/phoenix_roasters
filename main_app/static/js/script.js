7cd17c4f07962ef54f228054aedda52df2c4d661
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-23 19:22:14 -0400

    wip: typos in base/search html/overlay

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -2,3 +2,12 @@
 const shoppingcartsEditQuantity = document.getElementById("");
 const shoppingcartsAdd = document.getElementById("#shoppingcarts-add");
 const shoppingcartsForm = document.getElementById("#shoppingcarts-form");
+
+const overlay = $("#overlay");
+const searchBtn = $("#search-button");
+
+//
+searchBtn.on("click", function () {
+  $(".search").removeClass("search__hide");
+  overlay.addClass("overlay__show");
+});
