348ea07776d3d7bb2b514db989f6e82a128fcb96
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-24 17:39:10 -0400

    wip: home slider improved

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -1,51 +1,59 @@
 // variables
-
+const searchBtn = $("#search-button");
+const overlay = $("#overlay");
 //
-$(document).ready(function () {
-  $("#search-close").click(function () {
-    $("#search-overlay").fadeOut();
-    $("#search-button").show();
-  });
-  $("#search-button").click(function () {
-    $(this).hide();
-    $("#search-overlay").fadeIn();
-  });
+// $(document).ready(function () {
+//   $("#search-close").click(function () {
+//     $("#search-overlay").fadeOut();
+//     $("#search-button").show();
+//   });
+//   $("#search-button").click(function () {
+//     $(this).hide();
+//     $("#search-overlay").fadeIn();
+//   });
+// });
+searchBtn.on("click", function () {
+  $(".search").removeClass("search_low");
+  overlay.addClass("overlay_show");
 });
-// searchBtn.on("click", function () {
-//   $(".search").removeClass("search_low");
-//   overlay.addClass("overlay_show");
-// });
 
-// $(".navbar-toggler").on("click", function () {
-//   overlay.addClass("overlay_show");
-//   $("html").addClass("open-nav");
-// });
+$(".navbar-toggler").on("click", function () {
+  overlay.addClass("overlay_show");
+  $("html").addClass("open-nav");
+});
 
-// overlay.on("click", function () {
-//   $("search").addClass("search_low");
-//   overlay.removeClass("overlay_show");
-// });
+overlay.on("click", function () {
+  $("search").addClass("search_low");
+  overlay.removeClass("overlay_show");
+});
 
 $(".home_fav__products__slick").slick({
   infinite: true,
-  slideToShow: 4,
+  slidesToShow: 4,
   slidesToScroll: 1,
-  arrow: false,
+  arrows: false,
   dots: false,
   autoplay: true,
-  autoplaySpeed: 5000,
+  autoplaySpeed: 4500,
   responsive: [
     {
-      breakpoint: 550,
+      breakpoint: 600,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 990,
-      setting: {
-        slideToShow: 4,
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 985,
+      settings: {
+        slidesToShow: 4,
         slidesToScroll: 1,
       },
     },
